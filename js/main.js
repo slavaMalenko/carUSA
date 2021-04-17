deb2fc8328520239990b2e6cc2b7dc1e75f6d7eb
slavaMalenko <denouement20@yandex.ru>
2021-04-17 22:33:25 +0300

    adaptive version of the site

--- js/main.js
+++ js/main.js
@@ -5,7 +5,21 @@ $(function () {
         dots: true,
         slidesToShow: 3,
         autoplay: true,
-        autoplaySpeed: 2000
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 851,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }, {
+                breakpoint: 601,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+
+        ]
     })
 
 });
